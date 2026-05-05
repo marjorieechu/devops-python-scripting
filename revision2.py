8d77bf8c5cbb1add8754d24742f625989e8ecf9b
marjorieechu <marjbogo@gmail.com>
2026-05-05 09:53:10 +0100

    revision continued

--- revision2.py
+++ revision2.py
@@ -1,1 +1,23 @@
-print("My favourite food is egusi")
+fruits = ["apple", "banana", "cherry", "date", "elderberry"]
+print(fruits[0:5])  # Output: ['apple', 'banana', 'cherry', 'date', 'elderberry']
+
+tools = ["lambda", "s3", "load-balancer", "autoscaler"]
+print(tools[0:4])  # Output: ['lambda', 's3', 'load-balancer', 'autoscaler']
+
+print(fruits[0])  # Output: 'apple'
+print(tools[3])  # Output: 'autoscaler'
+print(fruits[0:2])  # Output: ['apple', 'banana']
+print(tools[2:4])  # Output: ['load-balancer', 'autoscaler']
+
+schools = ["ecwa", "havard", "cambridge"]
+schools.append("surebloom")
+print(schools)
+
+schools.remove("ecwa")
+print(schools)
+
+for fruit in fruits:
+    print(fruit)
+
+for tool in tools:
+    print(tool)
